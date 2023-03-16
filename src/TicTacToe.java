d5b617603d75d8ef72574bb3a722d6df27abcead
RazvanMacovei15 <razvanmc15@gmail.com>
2023-03-16 09:41:05 +0200

    implementing the matrix and font

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,19 +6,32 @@ import java.beans.EventHandler;
 
 
 public class TicTacToe extends JFrame implements ActionListener{
+    private JButton[][] buttons;
     private char currentPlayer ;
     public TicTacToe(){
         //create a jframe object
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 3));
-        for (int i = 0; i < 9; i++){
-            JButton button = new JButton("click me");
-            panel.add(button);
-            button.addActionListener( this);
+        //for (int i = 0; i < 9; i++){
+           // JButton button = new JButton("click me");
+            //panel.add(button);
+            //button.addActionListener( this);
+        buttons = new JButton[3][3];
 
+        Font font = new Font("Arial", Font.PLAIN,40);
+
+        for(int i = 0; i<3; i++){
+            for (int j = 0; j<3; j++){
+                buttons[i][j] = new JButton();
+                buttons[i][j].addActionListener(this);
+                buttons[i][j].setFont(font);
+                panel.add(buttons[i][j]);
+
+            }
 
         }
+        //add the panel to the frame
         frame.add(panel);
 
         //make the frame not resizeable
