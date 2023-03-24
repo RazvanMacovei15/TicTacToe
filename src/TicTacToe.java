15c376fd501e9bac52341e21c27a5b6b1932b1b7
RazvanMacovei15 <razvanmc15@gmail.com>
2023-03-24 16:20:07 +0200

    implementing line border on draw

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,6 +35,15 @@ public class TicTacToe extends JFrame implements ActionListener{
             }
 
         }
+        JPanel statusPanel = new JPanel();
+        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        frame.add(statusPanel, BorderLayout.SOUTH);
+        statusPanel.setPreferredSize(new Dimension(frame.getWidth(), 100));
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        JLabel statusLabel = new JLabel("status");
+        statusLabel.setFont(font);
+        statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.add(statusLabel);
         //add the panel to the frame
         frame.add(panel);
 
@@ -149,7 +159,6 @@ public class TicTacToe extends JFrame implements ActionListener{
         //}
         //I just learned I need to use a ternary operator to switch from 'x' to '0' and back around
         //currentPlayer = (currentPlayer == 'x') ? '0' : 'x';
-
 
     public static void main(String[] args) {
 
